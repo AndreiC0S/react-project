0e190031c0151cd9df7b848116d9f07e757457dd
AndreiC0S <andrei_cos97@yahoo.com>
2024-03-19 17:10:47 +0200

    test

--- backend/db.sql
+++ backend/db.sql
@@ -1,31 +1,26 @@
-CREATE SCHEMA `acwebsit_reactblog` DEFAULT CHARACTER SET utf8 ;
+CREATE SCHEMA `reactblog` DEFAULT CHARACTER SET utf8 ;
 
 
 --------------------------------------------------------------------------------
 
 --------------------------------------------------------------------------------
-
-CREATE TABLE `acwebsit_reactblog`.`admins` (
+CREATE TABLE `admins` (
   `id` int NOT NULL AUTO_INCREMENT,
   `username` varchar(450) NOT NULL,
   `password` varchar(450) NOT NULL,
   `email` varchar(450) NOT NULL,
+  `first_name` varchar(450) DEFAULT NULL,
+  `last_name` varchar(450) DEFAULT NULL,
+  `master` varchar(450) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `id_UNIQUE` (`id`),
   UNIQUE KEY `username_UNIQUE` (`username`),
   UNIQUE KEY `email_UNIQUE` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+
 
 -- 1.
-
-ALTER TABLE `acwebsit_reactblog`.`admins` 
-ADD COLUMN `master` TINYINT NULL AFTER `email`;
-
--- 2.
-ALTER TABLE `acwebsit_reactblog`.`admins` 
-CHANGE COLUMN `master` `master` VARCHAR(450) NULL ;
-
--- 3.
 INSERT INTO `admins` (`id`, `username`, `password`, `email`, `first_name`,`last_name`, `master`) VALUES (NULL, 'admin.root', '$2a$10$oQIQOXdTxDNxpOSyjYYhcuM.oepbdxwZIlxhrilz9VPzElT/PHAjm', 'admin@root.com','root', 'root', '1');
 -- password user: admin.root  email: admin@root.com  pass: Admin-root1
 
@@ -34,7 +29,7 @@ INSERT INTO `admins` (`id`, `username`, `password`, `email`, `first_name`,`last_
 
 --------------------------------------------------------------------------------
 
-CREATE TABLE `acwebsit_reactblog`.`products` (
+CREATE TABLE `products` (
   `id` int NOT NULL AUTO_INCREMENT,
   `nume_produs` varchar(200) NOT NULL,
   `descriere_produs` varchar(500) NOT NULL,
@@ -43,48 +38,40 @@ CREATE TABLE `acwebsit_reactblog`.`products` (
   `pret_produs` varchar(45) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `id_UNIQUE` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=34 DEFAULT CHARSET=utf8
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 --------------------------------------------------------------------------------
 
 --------------------------------------------------------------------------------
-
-CREATE TABLE `acwebsit_reactblog`.`users` (
+CREATE TABLE `users` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `email` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `nume` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `prenume` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `parola` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `dataadaugare` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `nume` varchar(255) NOT NULL,
+  `prenume` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `parola` varchar(255) NOT NULL,
+  `dataadaugare` datetime DEFAULT CURRENT_TIMESTAMP,
+  `country` varchar(255) DEFAULT NULL,
+  `county` varchar(255) DEFAULT NULL,
+  `city` varchar(255) DEFAULT NULL,
+  `address` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `email_UNIQUE` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8
-
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci
 --------------------------------------------------------------------------------
 
 --------------------------------------------------------------------------------
 
--- 1.
-CREATE TABLE `acwebsit_reactblog`.`orders` (
-  `id_orders` INT NOT NULL AUTO_INCREMENT,
-  `country` VARCHAR(70) NOT NULL,
-  `address` VARCHAR(4500) NOT NULL,
-  `items` VARCHAR(7000) NOT NULL,
-  `comments` VARCHAR(300) NULL,
+CREATE TABLE `orders` (
+  `id_orders` int NOT NULL AUTO_INCREMENT,
+  `country` varchar(70) NOT NULL,
+  `address` varchar(4500) NOT NULL,
+  `items` json NOT NULL,
+  `paid_card` int NOT NULL,
+  `comments` varchar(300) DEFAULT NULL,
+  `status` varchar(45) NOT NULL DEFAULT 'online',
   PRIMARY KEY (`id_orders`),
-  UNIQUE INDEX `id_orders_UNIQUE` (`id_orders` ASC) VISIBLE)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
-
--- 2.
-ALTER TABLE `acwebsit_reactblog`.`orders` 
-ADD COLUMN `paid_card` TINYINT NOT NULL DEFAULT 0 AFTER `items`,
-CHANGE COLUMN `country` `country` VARCHAR(70) NOT NULL ,
-CHANGE COLUMN `items` `items` JSON NOT NULL ;
-
---3.
-ALTER TABLE `acwebsit_reactblog`.`orders` 
-ADD COLUMN `status` VARCHAR(45) NOT NULL DEFAULT 'online' AFTER `comments`;
+  UNIQUE KEY `id_orders_UNIQUE` (`id_orders`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci
 
 
 --------------------------------------------------------------------------------
